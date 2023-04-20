61d247c2a53209bd663a7a8a2182be550699c1b0
sebadev-img <sebastiandev.img@gmail.com>
2023-04-19 21:49:34 -0300

    version 1.0

---
+++ script.js
@@ -0,0 +1,69 @@
+const inpFile = document.getElementById("inpFile");
+const textFile = document.getElementById("textFile");
+const btnUpload = document.getElementById("btnUpload");
+const resultText = document.getElementById("resultText");
+const ul = document.getElementById("ul");
+
+btnUpload.disabled = true;
+
+const getUniqueDNI = (text) => {
+  console.log("entro");
+  if (text) {
+    while (ul.firstChild) {
+      ul.removeChild(ul.firstChild);
+    }
+    const textArray = text.split(/\/|\s/);
+    //console.log("textarray", textArray);
+    let dniArray = textArray.filter((word) => /\d{8,}$/.test(word));
+    dniArray = dniArray.map((dni) => dni.slice(-8));
+    //console.log(dniArray);
+    const uniqueDNI = [...new Set(dniArray)].sort((a, b) => a - b);
+    console.log(uniqueDNI);
+    uniqueDNI.map((dni) => {
+      const node = document.createElement("li");
+      node.appendChild(document.createTextNode(dni));
+      node.addEventListener("click", () => {
+        node.classList.toggle("complete");
+      });
+      ul.appendChild(node);
+    });
+  }
+};
+
+inpFile.addEventListener("change", () => {
+  if (inpFile.value) {
+    textFile.innerHTML = inpFile.value.match(/[\/\\]([\w\d\s\.\-\(\)]+)$/)[1];
+    btnUpload.disabled = false;
+  } else {
+    textFile.innerHTML = "Ningun Archivo Seleccionado";
+  }
+});
+
+btnUpload.addEventListener("click", () => {
+  let textoPDF = "";
+  const file = inpFile.files[0];
+  const reader = new FileReader();
+  reader.readAsArrayBuffer(file);
+  reader.onload = () => {
+    const pdfData = new Uint8Array(reader.result);
+    pdfjsLib.getDocument({ data: pdfData }).promise.then((pdf) => {
+      const numPages = pdf.numPages;
+      let pagesText = [];
+
+      for (let i = 1; i <= numPages; i++) {
+        pdf.getPage(i).then((page) => {
+          page.getTextContent().then((textContent) => {
+            const pageText = textContent.items.map((item) => item.str).join("");
+            pagesText.push(pageText);
+            if (pagesText.length === numPages) {
+              const allText = pagesText.join("\n");
+              //console.log(allText);
+              textoPDF = allText;
+              getUniqueDNI(textoPDF);
+            }
+          });
+        });
+      }
+    });
+  };
+});
